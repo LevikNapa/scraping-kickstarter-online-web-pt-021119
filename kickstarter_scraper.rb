ff0256720f8284ce6ba0100a9f0765543a1faa6c
Levik Naparstek <levik010@gmail.com>
2019-03-27 19:59:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,9 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry 
+  
+  projects = {}
+  # binding.pry 
 end
 create_project_hash
 
